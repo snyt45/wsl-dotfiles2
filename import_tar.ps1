d403f905aa94ae61b8de425dc1b9c3dcbf9489c5
yuta.sano <snyt45@gmail.com>
2021-08-28 18:01:46 +0900

    import_tar.ps1を引数を渡して実行する形に修正

--- import_tar.ps1
+++ import_tar.ps1
@@ -1,1 +1,32 @@
 ﻿Write-Host "WSLにUbuntuをインポートします。"
+
+Param(
+  [String]$Arg1,
+  [String]$Arg2,
+  [String]$Arg3
+)
+
+$DistroName = $Arg1
+$InstallLocation = $Arg2
+$InstallTarFile = $Arg3
+
+# wslコマンドが存在するか
+try {
+  Get-Command -Name wsl -ErrorAction Stop
+} catch {
+  Write-Host "wslコマンドが存在しません。"
+  exit 1
+}
+
+# tarファイルがあるか
+try {
+  if (!(Test-Path $InstallTarFile)) { throw }
+} catch {
+  Write-Host "指定されたtarファイルが見つかりません。"
+  exit 1
+}
+
+# wsl --import <DistroName> <InstallLocation> <InstallTarFile>
+wsl --import $DistroName $InstallLocation $InstallTarFile
+
+Write-Host "Done."
