c0e5eb3e194666575f6d1898c8feac26135e8ffb
yuta.sano <snyt45@gmail.com>
2022-01-01 17:32:28 +0900

    シンボリックリンク配置のシェルスクリプトから不要になった処理を削除

--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -14,18 +14,6 @@ set -Ceu
 
 # ~/.vim
 mkdir -p ~/.vim
-ln -snfv ~/.dotfiles/.vim/vim-plug.vim ~/.vim/
 ln -snfv ~/.dotfiles/.vim/vimrc ~/.vim/
-
-# ~/.vim/plugin-config
-mkdir -p ~/.vim/plugin-config
-ln -snfv ~/.dotfiles/.vim/plugin-config/blammer.vim ~/.vim/plugin-config/
-ln -snfv ~/.dotfiles/.vim/plugin-config/fern.vim ~/.vim/plugin-config/
-ln -snfv ~/.dotfiles/.vim/plugin-config/fzf.vim ~/.vim/plugin-config/
-ln -snfv ~/.dotfiles/.vim/plugin-config/indentLine.vim ~/.vim/plugin-config/
-ln -snfv ~/.dotfiles/.vim/plugin-config/vim-floaterm.vim ~/.vim/plugin-config/
-ln -snfv ~/.dotfiles/.vim/plugin-config/vim-gitgutter.vim ~/.vim/plugin-config/
-ln -snfv ~/.dotfiles/.vim/plugin-config/vim-which-key.vim ~/.vim/plugin-config/
-ln -snfv ~/.dotfiles/.vim/plugin-config/vimdoc-ja.vim ~/.vim/plugin-config/
 
 echo "Done."
